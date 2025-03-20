eac3ed0cd57340880eb9ea89fefbd39d9dd7a8ba
Siva Rama Krishna Prasad Changala <changala.2@wright.edu>
2025-03-20 09:17:42 -0400

    figured vscode extension for springboot extension pack not working and cassandra dependenc error has been resolved manually

--- src/main/java/io/javabrains/InboxApp.java
+++ src/main/java/io/javabrains/InboxApp.java
@@ -1,25 +1,41 @@
 package io.javabrains;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
 public class InboxApp {
 
+    /*
+	private final CqlSessionBuilderCustomizer sessionBuilderCustomizer;
+
+    private final SecurityAdapter securityAdapter;
+
+
+    InboxApp(SecurityAdapter securityAdapter, CqlSessionBuilderCustomizer sessionBuilderCustomizer) {
+        this.securityAdapter = securityAdapter;
+        this.sessionBuilderCustomizer = sessionBuilderCustomizer;
+    }
+	*/
+
 	public static void main(String[] args) {
 		SpringApplication.run(InboxApp.class, args);
 	}
 
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
+
+	
 	
 
 }
