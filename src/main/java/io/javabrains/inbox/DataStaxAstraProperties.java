49fe472b8544047f8c6c8671b52b82b2c89ce4e4
Siva Rama Krishna Prasad Changala <changala.2@wright.edu>
2025-03-24 09:47:17 -0400

    Implement initial structure for Inbox application with security and folder management

--- src/main/java/io/javabrains/inbox/DataStaxAstraProperties.java
+++ src/main/java/io/javabrains/inbox/DataStaxAstraProperties.java
@@ -1,10 +1,12 @@
-package io.javabrains;
+package io.javabrains.inbox;
 
 import java.io.File;
+import org.springframework.stereotype.Component;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "datastax.astra")
+@Component
 public class DataStaxAstraProperties {
     private File secureConnectBundle;
 
--- src/main/java/io/javabrains/inbox/InboxApp.java
+++ src/main/java/io/javabrains/inbox/InboxApp.java
@@ -1,12 +1,18 @@
-package io.javabrains;
+package io.javabrains.inbox;
 
 import java.nio.file.Path;
+
+import javax.annotation.PostConstruct;
+
+import io.javabrains.inbox.folders.Folder;
+import io.javabrains.inbox.folders.FolderRepository;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @SpringBootApplication
@@ -25,6 +31,8 @@ public class InboxApp {
     }
 	*/
 
+	@Autowired FolderRepository folderRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(InboxApp.class, args);
 	}
@@ -33,6 +41,13 @@ public class InboxApp {
 	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
 		Path bundle = astraProperties.getSecureConnectBundle().toPath();
 		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+	@PostConstruct
+	public void init() {
+		folderRepository.save(new Folder("cleancoder1016","Inbox", "orange"));
+		folderRepository.save(new Folder("cleancoder1016","Sent", "blue"));
+		folderRepository.save(new Folder("cleancoder1016","Important", "green"));
 	}
 
 	
